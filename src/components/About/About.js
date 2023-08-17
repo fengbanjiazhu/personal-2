fa67c323c4228f352885c9d49293b48d5cbfba85
Jeffrey <562475805lanu@gmail.com>
2023-08-17 21:26:07 +1000

    removes unwanted images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,6 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
 import TimeLine from "./TimeLine";
 
@@ -28,7 +27,6 @@ function About() {
             <Aboutcard />
           </Col>
           <Col md={5} style={{ paddingTop: "120px", paddingBottom: "50px" }} className="about-img">
-            {/* <img src={laptopImg} alt="about" className="img-fluid" /> */}
             <TimeLine />
           </Col>
         </Row>
