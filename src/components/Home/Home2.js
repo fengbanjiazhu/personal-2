bb9714b7d2accb98cb5f5e016f02253f259dfad8
Jeffrey <562475805lanu@gmail.com>
2023-10-25 17:21:21 +1100

    removes Link import

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Tooltip } from "antd";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
